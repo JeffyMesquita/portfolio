26b7ad661a0f7b8c418d5bb1f62040a21e52ec2b
Jeferson Mesquita <je_2742@hotmail.com>
2023-07-10 17:08:40 -0300

    done

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,36 @@ export default function Home() {
       <Tools />
       <div className="box w-10/12 mx-auto max-md:mt-[-50px] mt-8 mb-[-40px]" />
       <Projects />
+      <article className="w-10/12 mx-auto my-10">
+        <div className="flex items-start text-2xl text-neutral-300">
+          <p className="text-5xl mr-2 mt-[2px]">&#169;</p> Jeferson Mesquita{" "}
+          <small className="mt-[2px] ml-2">2023</small>{" "}
+          <p className="text-2xl mr-2 mt-[2px]">|</p>
+          <a
+            href="https://github.com/JeffyMesquita"
+            className="text-blue-600 hover:underline"
+            target="_blank"
+          >
+            Github
+          </a>
+          <p className="text-2xl mr-2 mt-[2px]">|</p>
+          <a
+            href="https://www.linkedin.com/in/jeferson-mesquita-763bb6b8/"
+            className="text-blue-600 hover:underline"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+          <p className="text-2xl mr-2 mt-[2px]">|</p>
+          <a
+            href="https://www.instagram.com/jeferson.mesquita/"
+            className="text-blue-600 hover:underline"
+            target="_blank"
+          >
+            Instagram
+          </a>
+        </div>
+      </article>
     </main>
   );
 }
