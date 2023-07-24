428b1079919f5993c1aaf9f832a725fb29cff356
Jeferson Mesquita <je_2742@hotmail.com>
2023-07-24 16:39:12 -0300

    📇feat:: SEO

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,33 +1,50 @@
-import Image from "next/image";
 import { ImageResponse } from "next/server";
 
 export const size = {
-  width: 900,
-  height: 450,
+  width: 1200,
+  height: 630,
 };
+export const alt = "Jeferson Mesquita, meu portfólio ";
 
 export const contentType = "image/png";
 
-// interface Props {
-//   params: {
-//     slug: string;
-//   }
-// }
+// Font
+const interSemiBold = fetch(
+  new URL("./Inter-SemiBold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
 
-// Caso você queira que a imagem seja dinâmica, você pode usar o código abaixo:
-
-export default async function og() {
+// Image generation
+export default async function Image() {
   return new ImageResponse(
     (
-      <div tw="relative flex items-center justify-center">
-        <Image src="/jeferson.png" alt="Jeferson Mesquita" />
-        <div tw="absolute flex bg-black opacity-50 inset-0" />
-        <div tw="absolute flex items-center top-2 w-full">
-          <p tw="text-white text-4xl flex font-bold m-5">Jeferson Mesquita</p>
-          <p tw="text-indigo-200 text-xl flex font-bold m-5">Meu Portfólio</p>
-        </div>
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Jeferson Mesquita, meu portfólio
       </div>
     ),
-    size
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: await interSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }
   );
 }
