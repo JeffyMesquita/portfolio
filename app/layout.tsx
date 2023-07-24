730cb703d0b66c91c381e0156ba77ebc1afa6654
Jeferson Mesquita <je_2742@hotmail.com>
2023-07-24 15:56:41 -0300

    fix???

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Space_Mono } from "next/font/google";
+import "./globals.css";
 
 const space_mono = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://jeffymesquita.dev"),
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    images: "/og-image.png",
+  },
   title: {
     default: "Jeferson Mesquita",
     template: "Jeferson Mesquita | %s",
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 export default async function sitemap() {
-  const baseUrl = "https://www.example.com";
+  const baseUrl = "https://jeffymesquita.dev";
 
   return [{ url: baseUrl, lastModified: new Date() }];
 }
